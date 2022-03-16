c8c340b58c64c1d441d1e957c4cf75d962d038f2
ParthTrehan22 <63785074+ParthTrehan22@users.noreply.github.com>
2022-03-17 00:37:12 +0530

    Added full functionality

--- src/App.js
+++ src/App.js
@@ -3,19 +3,12 @@ import Stopwatch from "./Stopwatch";
 import { useEffect, useState } from "react";
 
 export default function App() {
-  const [time, setTime] = useState([{ h: 0, m: 0, s: 0, ms: 0 }]);
   const [stopwatches, setStopwatches] = useState([]);
   useEffect(() => {
-    setStopwatches((prevState) => [
-      ...prevState,
-      <Stopwatch time={time}></Stopwatch>
-    ]);
+    setStopwatches((prevState) => [...prevState, <Stopwatch></Stopwatch>]);
   }, []);
   let handleAdd = function () {
-    setStopwatches((prevState) => [
-      ...prevState,
-      <Stopwatch time={time}></Stopwatch>
-    ]);
+    setStopwatches((prevState) => [...prevState, <Stopwatch></Stopwatch>]);
   };
   return (
     <div className="App">
--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,12 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Stopwatch(props) {
+  const [time, setTime] = useState([{ h: 0, m: 0, s: 0, ms: 0 }]);
+  var updatedh = 0,
+    updatedm = 0,
+    updateds = 0,
+    updatedms = 0;
+  useEffect(() => {
+    setInterval(() => {
+      updatedms++;
+      if (updatedms > 1000) {
+        updateds++;
+        updatedms = 0;
+      }
+      if (updateds > 60) {
+        updatedm++;
+        updateds = 0;
+      }
+      if (updatedm > 60) {
+        updatedh++;
+        updatedm = 0;
+      }
+      setTime({ h: updatedh, m: updatedm, s: updateds, ms: updatedms });
+    }, 1);
+  }, []);
   return (
     <div>
-      {props.time[0].h > 9 ? props.time[0].h : "0" + props.time[0].h}:
-      {props.time[0].m > 9 ? props.time[0].m : "0" + props.time[0].m}:
-      {props.time[0].s > 9 ? props.time[0].s : "0" + props.time[0].s}:
-      {props.time[0].ms > 9 ? props.time[0].ms : "0" + props.time[0].ms}
+      {time.h > 9 ? time.h : "0" + time.h}:{time.m > 9 ? time.m : "0" + time.m}:
+      {time.s > 9 ? time.s : "0" + time.s}:
+      {time.ms > 9 ? time.ms : "0" + time.ms}
     </div>
   );
 }
